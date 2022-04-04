1cecaaa05af2567ce1ad4ce51a03100fc583b728
Andrea Cordoba <dandrea.cordoba@gmail.com>
2022-04-04 23:32:14 +0200

    progress exercises 1,2,3 hook module

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,15 +2,46 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
-
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  // extra credit 1
+  function getInitialValue () {
+   return  window.localStorage.getItem('name') ?? initialName;
+  }
+  const [name, setName] = React.useState(getInitialValue);
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name])
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+// extra credit 3
+const useLocalStorageState = (key, defaultValue = '') => {
+  function getInitialValue () {
+    const currentValue = window.localStorage.getItem(key);
+    return JSON.parse(currentValue ?? defaultValue);
+  }
+  const [value, setValue] = React.useState(getInitialValue);
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }, [value])
+  return [value, setValue]
+}
+
+function GreetingCustomHook({initialName = ''}) {
+  const [name, setName] = useLocalStorageState("name", initialName);
 
   function handleChange(event) {
     setName(event.target.value)
@@ -27,7 +58,10 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <>
+    <Greeting />
+    <GreetingCustomHook />
+    </>
 }
 
 export default App
